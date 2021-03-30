2550fad6dee0c07224163384460662d1abdb3539
Angela Conde <56882332+angelaconde@users.noreply.github.com>
2021-03-30 19:36:59 +0200

    Modified default UI Auth User model and migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id()->autoIncrement();
             $table->string('name');
+            $table->string('avatar')->default('user.jpg');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('provider');
+            $table->string('provider_id');
         });
     }
 
