7943f806aa647b43fa654c186cc8fbb93d571615
Angela Conde <56882332+angelaconde@users.noreply.github.com>
2021-03-31 11:34:56 +0200

    Added game model and migration

---
+++ app/Models/Game.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'api_id',
+        'name',
+        'summary',
+        'first_release_date',
+        'cover',
+        'platform',
+        'screenshot_1',
+        'screenshot_2',
+        'video'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'api_id',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'first_release_date' => 'datetime',
+    ];
+}
---
+++ database/migrations/2021_03_31_090615_create_games_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->timestamps();
+            $table->integer('api_id')->unique();
+            $table->string('name');
+            $table->string('summary')->nullable();
+            $table->timestamp('first_release_date')->nullable();
+            $table->string('cover')->nullable();
+            $table->string('platform')->default('PS4');
+            $table->string('screenshot_1')->nullable();
+            $table->string('screenshot_2')->nullable();
+            $table->string('video')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
