33aa3b13e73a9916cd99ffe99ba9e720dc9321b2
Angela Conde <56882332+angelaconde@users.noreply.github.com>
2021-04-07 19:23:41 +0200

    Custom 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="jumbotron jumbotron m-2">
+            <div class="container">
+                <h1 class="text-center">Sorry, there's nothing here.</h1>
+                <h5 class="text-center">Your fault, our fault, we'll never know...</h5>
+            </div>
+        </div>
+    </div>
+@endsection
